a0d93977644a3043681f9bbd8d9ebb5042061934
Lucas Gomide <lgomide@uplexis.com.br>
2013-09-05 12:47:30 -0300

    Convertendo texto e parseando a string

--- SpiderMan.py
+++ SpiderMan.py
@@ -11,30 +11,47 @@ from pyquery import PyQuery
 
 class SpiderMan:
     
-    urls = ('http://www.uol.com.br',)
+    urls = ('http://www.uol.com.br','http://www.globo.com')
     
+    
+    def encondingUTF_8(self, str):
+        try:
+            return str.encode("utf-8")
+        except Exception as e:
+            print ("Impossível mudar a codificação do HTML", e)
+    def getDominio(self, url):
+        try:
+            url = url.split('.')
+            return url[1].upper()
+        except Exception as e:
+            print "Não foi possível parsear a URL " ,e
+            
     #request GET para todas as urls, e retorna o html para o método def
     def get(self):
         try:
             for url in self.urls:
                 try:
                     html = requests.get(url).content
+                    print "Noticias do(a): ", self.getDominio(url)
                     html = PyQuery(html)
                     self.parse(html)
                 except:
                     print ('Erro na requisição da página: ', url)
         except:
             print ("Não foi possíve completar a requisição às páginas")
-    
+            
     #parsea as principais notícias dos blogs, retorna os itens parseados
     def parse(self, html):
         try:
             pq = PyQuery(html)
             results = pq('div.moduloChamada > h1 > a')
+            
             for result in results:
-                print pq(result).text(), "\n"
-        except:
-            print ("Não foi possível parsear o HTML")
+                news = pq(result).text()
+                print self.encondingUTF_8(news)
+            print "------------------------------------------"
+        except Exception as e:
+            print "Não foi possível parsear o HTML " ,e
             
     #método principal que execulta             
     def search(self):
